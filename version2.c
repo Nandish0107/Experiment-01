3287813e7c509ac2722619a205aa8f92a463ed1f
Nandish0107 <nandumelvin17@gmail.com>
2025-01-09 11:17:50 +0530

    version2.c

---
+++ version2.c
@@ -0,0 +1,13 @@
+#inlcude<stdio.h>
+#include <stdio.h>
+
+int main() {
+    int a = 41, b = 32, c = 25, x = 2;
+    int y;
+
+    y = a * x * x + b * x + c;
+
+    printf("The predicted value is %d units\n", y);
+
+    return 0;
+}
